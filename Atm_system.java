01c59377a7266df4dcf7e1ea5d8b54c94d8aa15b
satyatorati999-cmr <your-email@example.com>
2026-06-07 14:13:23 +0530

    Commit ATM Project Code

---
+++ Atm_system.java
@@ -0,0 +1,71 @@
+import java.util.Scanner;
+
+public class Atm_system {
+    static double balance = 10000;
+    //Check Balance 
+    public static void CheckBalance(){
+        System.out.println("Your balance is : " + balance);
+    }
+
+    //Withdrawl
+    public static void Withdrawl(double amount){
+        if(amount <=0){
+            System.out.println("Invalid amount");
+        }
+        else if(balance < amount){
+            System.out.println("Insufficient Balance");
+        }
+        else{
+            balance = balance - amount;
+            System.out.println("You have withdrawn : " + amount);
+            System.out.println("Your balance is : " + balance);
+        }
+        
+    }
+    public static void Deposit(double amount){
+        if(amount <=0){
+            System.out.println("Invalid amount");
+        }
+        else{
+            balance = balance + amount;
+            System.out.println("You have deposited : " + amount);
+            System.out.println("Your balance is : " + balance);
+        }
+    }
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        while (true) {
+
+            System.out.println("\n===== ATM MENU =====");
+            System.out.println("1. Check Balance");
+            System.out.println("2. Deposit");
+            System.out.println("3. Withdraw");
+            System.out.println("4. Exit");
+
+            System.out.print("Enter Choice: ");
+            int choice = sc.nextInt();
+
+            switch(choice){
+                case 1:
+                    CheckBalance();
+                    break;
+                case 2:
+                    System.out.print("Enter amount to deposit: ");
+                    double amount = sc.nextDouble();
+                    Deposit(amount);
+                    break;
+                case 3:
+                    System.out.print("Enter amount to withdraw: ");
+                    amount = sc.nextDouble();
+                    Withdrawl(amount);
+                    break;
+                case 4:
+                    System.out.println("Thank you for using ATM");
+                    System.exit(0);
+                default:
+                    System.out.println("Invalid Choice");
+            }
+        }
+    }
+}
--- Calculator.java
+++ Calculator.java
@@ -1,7 +1,6 @@
 import java.util.Scanner;
 
 public class Calculator {
-
     public static double add(double a , double b) {
         return a + b;
     }
